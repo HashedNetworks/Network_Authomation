ccf939af5af02fc0550d796359d8173208add38d
HashedNetworks <96129893+HashedNetworks@users.noreply.github.com>
2022-11-03 15:31:26 -0400

    inve loop.py

---
+++ inve loop.py
@@ -0,0 +1,48 @@
+import yaml
+
+
+def get_inventory_dic():
+    with open('/home/andreoss/network_automation/Python Scripts/Network Automation/inventory.yml', 'r') as inventory_dic:
+        try:
+        # Converts yaml document to python object
+            invent_dic=yaml.safe_load(inventory_dic)
+        
+        # Printing dictionary
+            #print(d)
+        except yaml.YAMLError as e:
+            print(e)
+    
+    return invent_dic  
+
+
+# Finds IP and IOS code for given hostname 
+def get_ip_code(hostname:list, **invent_dic):
+    
+    for num_hn in range(len(hostname)):
+        for platform, value in invent_dic['devices'].items():
+                for hn in invent_dic['devices'][platform].keys():
+                    if hn == hostname[num_hn]:
+
+                        host = invent_dic['devices'][platform][hn]['host']
+                        device_type = invent_dic['devices'][platform][hn]['device_type']
+
+                        device = {
+                            'device_type' : device_type,
+                            'host' : host,
+                            'username' : 'agarcia',
+                            'password' : 'cisco123',
+                            }
+
+                        print(device)
+
+
+                        return device
+
+def main():
+
+    hostname= ['csr', 'sleaf-01']
+    invent_dic = get_inventory_dic()
+    ip_code = get_ip_code(hostname, **invent_dic)
+
+
+main()

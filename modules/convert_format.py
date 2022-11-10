e372bf480dbaf9c4512d33acd9f30356347d1983
Julian Naranjo <julian.naranjom@gmail.com>
2022-11-09 21:48:35 -0500

    Add docstring formatting

--- modules/convert_format.py
+++ modules/convert_format.py
@@ -8,6 +8,11 @@ import json
 from module_utils.open_file import open_filename
 
 def yaml_to_dict(filename):
+    """ Function to load YAML object to a Python object
+
+    Args:
+        filename (str): filename with YAML data that will be converted to Python object
+    """
     data = open_filename(filename)
     dict = yaml.safe_load(data)
     print ("From YAML TO DICT: \n")
@@ -15,6 +20,11 @@ def yaml_to_dict(filename):
     print ("----------------\n")
 
 def json_to_dict(filename):
+    """ Function to load JSON object to a Python object
+
+    Args:
+        filename (str): filename with JSON data that will be converted to Python object
+    """
     data = open_filename(filename)
     print ("From JSON TO DICT: \n")
     dict = json.loads(data)

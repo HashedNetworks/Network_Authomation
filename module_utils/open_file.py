b2809a8579e297eab4bf3605340f59c9d2c9d956
Julian Naranjo <julian.naranjom@gmail.com>
2022-11-09 21:54:35 -0500

    Add docstring formatting

--- module_utils/open_file.py
+++ module_utils/open_file.py
@@ -1,6 +1,14 @@
 """ Function to open a file """
 
 def open_filename(filename):
+    """ Function to open a file and loaded it into a variable
+
+    Args:
+        filename (str): filename with input data that will be converted to Python object
+
+    Returns:
+        dict: input data to convert
+    """
     with open(filename, 'r') as filehandle:
         data = filehandle.read()
         print ("ORIGINAL FILE FORMAT: \n")
